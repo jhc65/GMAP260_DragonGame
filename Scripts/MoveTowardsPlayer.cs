44fd0819de091762f778fb20007efaf24b3cc9c4
Jason <jw3345@drexel.edu>
2017-10-27 00:03:14 -0400

    MoveTowardsPlayer script

---
+++ Scripts/MoveTowardsPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MoveTowards : MonoBehaviour {
+    private Transform target;
+    public float speed = 5f;
+
+    void Start () {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+   
+
+	void Update () {
+        
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+}

f826d13cbe169aef2938aabb442530d34549c3ed
Jason <jw3345@drexel.edu>
2017-10-27 04:22:20 -0400

    Movement script and new scene

--- Scripts/MoveTowardsPlayer.cs
+++ Scripts/MoveTowardsPlayer.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveTowards : MonoBehaviour {
+public class MoveTowardsPlayer : MonoBehaviour {
+
     private Transform target;
     public float speed = 5f;
 
     void Start () {
         target = GameObject.FindGameObjectWithTag("Player").transform;
     }
-   
+	
 
 	void Update () {
-        
         transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
     }
 }
---
+++ SnifflesDontPlayGames/Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float playerSpeed = 4f;
+
+	
+	void Start () {
+		
+	}
+	
+
+	void FixedUpdate () {
+
+        Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        GetComponent<Rigidbody2D>().velocity = targetVelocity * playerSpeed;
+    }
+}

1e6fb989d4bfe2ee60d5c9f6d1e0c3a2b98e6eca
Ben Schenerman <ben.schenerman@gmail.com>
2017-10-31 15:18:01 -0400

    canShoot prop in shootController now modified by DisableShooting fn

--- SnifflesDontPlayGames/Assets/Scripts/ShootController.cs
+++ SnifflesDontPlayGames/Assets/Scripts/ShootController.cs
@@ -6,13 +6,19 @@ public class ShootController : MonoBehaviour {
 
 	public GameObject bulletPrefab;
 
+
 	private static int numBullets = 30;
 	private GameObject[] bullets = new GameObject[numBullets];
 	private int next;
 	private float bulletVelocity = 20.0f;
 	private GameObject currentBullet;
 	private float bulletSpawnOffset = 0.5f;
-	public bool canShoot = true;
+	private bool canShoot = true;
+
+	public void DisableShooting()
+	{
+		canShoot = false;
+	}
 
 	// Init array of bullets
 	void Start () {

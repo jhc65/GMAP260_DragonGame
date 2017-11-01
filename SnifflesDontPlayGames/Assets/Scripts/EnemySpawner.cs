8cefd801cc332cc34222d40cccee9c1f203de914
Mariah Sheehan <mks94@drexel.edu>
2017-10-31 21:39:29 -0400

    modified:   Assets/Prefabs/enemy.prefab

---
+++ SnifflesDontPlayGames/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+	INSTRUCTIONS TO USE
+
+	Attach this script to any game object in the scene, preferably
+	  an empty game object called EnemySpawner or similar
+
+	Click on the game object, and drag the enemy prefab onto the
+	  ENEMY_TO_SPAWN parameter
+	
+	Expand the SPAWN_POINTS parameter, changing the Size to the number
+	  of desired spawn points.  For each point, enter the coordinates
+	
+	Change the SPAWN_FREQUENCY to the desired time between enemy spawns
+*/
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject ENEMY_TO_SPAWN;
+	public Vector3[] SPAWN_POINTS;
+	public float SPAWN_FREQUENCY = 5.0F;
+
+	private float _timeUntilNextSpawn = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_timeUntilNextSpawn <= 0.0f && SPAWN_POINTS.Length > 0)
+		{
+			int i = Random.Range(0, SPAWN_POINTS.Length);
+			Vector3 spawnPosition = SPAWN_POINTS[i];
+			GameObject spawnedEnemy = GameObject.Instantiate(ENEMY_TO_SPAWN);
+			spawnedEnemy.transform.position = spawnPosition;
+			_timeUntilNextSpawn = SPAWN_FREQUENCY;
+		}
+		else
+		{
+			_timeUntilNextSpawn -= Time.deltaTime;
+		}
+	}
+}

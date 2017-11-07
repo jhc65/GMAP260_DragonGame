f7c8b64c58e454c44e2509c4dc2757a756bd9af6
CoreDump <cmb539@drexel.edu>
2017-11-06 20:06:00 -0500

    Enemy spawner now spawns groups of enemies.

--- SnifflesDontPlayGames/Assets/Scripts/EnemySpawner.cs
+++ SnifflesDontPlayGames/Assets/Scripts/EnemySpawner.cs
@@ -15,6 +15,8 @@ public class EnemySpawner : MonoBehaviour {
 	public GameObject enemy;
 	public Vector3[] spawnPoints;
 	public float frequency = 5.0F;
+	public int maxToSpawn = 4;
+	public float spreadDistance = 20.0f;
 
 	private float spawnCooldown = 0.0f;
 	private bool enabled = true;
@@ -28,11 +30,36 @@ public class EnemySpawner : MonoBehaviour {
 			return;
 		
 		if (spawnCooldown <= 0.0f && spawnPoints.Length > 0) {
-			int i = Random.Range(0, spawnPoints.Length); // random spawn point
-			Vector3 spawnPosition = spawnPoints[i];
-			GameObject spawnedEnemy = GameObject.Instantiate(enemy);
-			spawnedEnemy.transform.position = spawnPosition;
-            spawnCooldown = gameController.GetEnemySpawnFrequency();
+			int numToSpawn = Random.Range(1, maxToSpawn);
+			int spawnPointIndex = Random.Range(0, spawnPoints.Length); // random spawn point
+			
+			for (int i = 0; i < numToSpawn; i++)
+			{
+				Vector3 spawnPosition = spawnPoints[spawnPointIndex];
+				GameObject spawnedEnemy = GameObject.Instantiate(enemy);
+				Vector3 positionToMove = spawnPosition;
+				switch (i % 5)
+				{
+					case 1:
+						positionToMove += spreadDistance * Vector3.up;
+						break;
+					case 2:
+						positionToMove += spreadDistance * Vector3.right;
+						break;
+					case 3:
+						positionToMove += spreadDistance * Vector3.down;
+						break;
+					case 4:
+						positionToMove += spreadDistance * Vector3.left;
+						break;
+						
+					default:
+
+						break;
+				}
+				spawnedEnemy.transform.position = positionToMove;
+				spawnCooldown = gameController.GetEnemySpawnFrequency();
+			}
 		}
 		else {
 			spawnCooldown -= Time.deltaTime;

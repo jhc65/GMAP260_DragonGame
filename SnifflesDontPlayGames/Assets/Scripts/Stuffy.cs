a8646d09dfa5d72ab76e15b074505f642888a3b2
Daniel Perlman <dmp359@drexel.edu>
2017-11-14 23:48:41 -0500

    Updated sprites and stuffy bbug fix to prevent one thief from picking up two stuffies

--- SnifflesDontPlayGames/Assets/Scripts/Stuffy.cs
+++ SnifflesDontPlayGames/Assets/Scripts/Stuffy.cs
@@ -20,12 +20,14 @@ public class Stuffy : MonoBehaviour {
 
 		// If a thief touches the stuffy and it has a parent (is a part of the stuffy pile), hold it
 		if (collision.gameObject.CompareTag("Stealer")) {
+			if (collision.gameObject.transform.childCount > 0) 
+				return;
             newParent = collision.transform;
             transform.parent = newParent; //changes stuffy parent
 			objHolding = collision.gameObject;
 			objHolding.GetComponent<MoveTowardStuffies>().SetIsHoldingItem(true);
 		}
-			
+
 		// If a player touches the stuffy and it has no parent (alone :( ), return it
 		if (collision.gameObject.CompareTag("Player") && transform.parent == null) {
 			ReturnStuffy();
